a5279d33f3024989516cfe290dc3808bde201fe0
Chris Waddington <cwadding@gmail.com>
2013-12-18 11:25:39 -0500

    created arecont adapter

--- app/helpers/camera_scanner/cam_api/arecont.js
+++ app/helpers/camera_scanner/cam_api/arecont.js
@@ -1,9 +1,17 @@
+var request = require('request');
+
 var baseUrl = 'http://{user}:{pass}@{ip}';
 var rtspUrl = 'rtsp://{user}:{pass}@{ip}/h264.sdp?res={resolution}&fps={framerate}';
 
-var getRtspUrl = function ( cam, profile ) {
+function Arecont( cam ){
+	this.username = cam.username;
+	this.password = cam.password;
+	this.ip = cam.ip;
+};
+
+Arecont.prototype.getRtspUrl = function (profile ) {
 	
-	if (!cam || !profile) return;
+	if (!profile) return;
 	
 	var dimensions = profile.resolution.split('x');
 	var width = dimensions[0];
@@ -17,41 +25,108 @@ var getRtspUrl = function ( cam, profile ) {
 	}
 
 	return rtspUrl
-		.replace('{user}', cam.user)
-		.replace('{pass}', cam.password)
-		.replace('{ip}', cam.ip)
+		.replace('{user}', this.username)
+		.replace('{pass}', this.password)
+		.replace('{ip}', this.ip)
 		.replace('{resolution}', res)
 		.replace('{framerate}', profile.framerate);
 };
 
-var setMotionParams = function(params){
-	// "/set?motiondetect=on"
+Arecont.prototype.cameraUrl = function () {
+	return baseUrl
+		.replace('{user}', this.username)
+		.replace('{pass}', this.password)
+		.replace('{ip}', this.ip);
 };
 
-var getMotionParams = function(){
+Arecont.prototype.setMotionParams = function(params){
+	var urlParams= [];
+	if (params.enabled){
+		urlParams.push("motiondetect=" + (params.enabled ? "on" : "off") );
+	}
+	if (params.threshold){
+		urlParams.push("mdlevelthreshold=" + params.threshold );
+	}
+	if (params.sensitivity){
+		urlParams.push("mdsensitivity=" + params.sensitivity );
+	}
+
+	var url = this.cameraUrl() + "/get?" + urlParams.join("&");
+	request({ 
+		url: url,
+		headers: {
+			'User-Agent': 'nodejs'
+		},
+	}, function( error, response, body) {
+			if (!error && body) {
+
+			}else{
+
+			}
+		}
+	);
+};
+
+Arecont.prototype.getParam = function(name){
+	var value = "";
+	var self = this;
+	var digest = new Buffer(this.username + ":" + this.password).toString('base64');
+	request({ 
+		url: self.cameraUrl() + "/get?" + name + "&id=" + Date.now(),
+		headers: {
+			'User-Agent': 'nodejs',
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+			'Authorization': 'Basic ' + digest
+		},
+	}, function( error, response, body) {
+		
+			if (!error && body) {
+				var ele = body.toString().split("=");
+				value = ele[1];
+				return value;
+			}else{
+
+			}
+		}
+	);
 	
 };
 
-var isMotionEnabled = function(){
+Arecont.prototype.getMotionParams = function(){
+
+	return {
+		enabled: this.isMotionEnabled(),
+		threshold: parseInt(this.getParam("mdlevelthreshold")), 
+		sensitivity: parseInt(this.getParam("mdsensitivity"))};
+};
+
+Arecont.prototype.isMotionEnabled = function(){
+	return this.getParam("motiondetect") == "on" ? true : false
+};
+
+Arecont.prototype.setupMotionDetection = function(){
+	if (!isMotionEnabled()){
+		setMotionParams({enabled: true})
+	}
 
 };
 
-var setupMotionDetection = function(cam){
-	// check 
-	// enable motion detection
-
+Arecont.prototype.startListeningForMotionDetection = function(cb){
+	var self = this;
+	console.log(self.cameraUrl() + "/get?" + 'mdresult');
+	self.process_id = setInterval(function(){
+		result = self.getParam('mdresult');
+		if (result !== 'no motion'){
+			cb();
+		}
+	},100);
 };
 
-var startListeningForMotionDetection = function(cam, cb){
-	//poll
-		//emit motion
-};
-
-var stopListeningForMotionDetection = function(){
-	//poll
-		//emit motion
+Arecont.prototype.stopListeningForMotionDetection = function(){
+		clearInterval(this.process_id);
 };
 
 
-exports.getRtspUrl = getRtspUrl;
+// exports.getRtspUrl = getRtspUrl;
 
+module.exports = Arecont;
