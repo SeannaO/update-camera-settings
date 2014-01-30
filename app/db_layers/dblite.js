1ff94e6907803fea08c3774143c13fc568a6408d
Manuel Guerra <manuel@macroMundo2.local>
2014-01-30 14:18:49 -0500

    corrects a bug in dblite.getExpiredChunks wich prevented correct filenames to be returned

--- app/db_layers/dblite.js
+++ app/db_layers/dblite.js
@@ -139,7 +139,7 @@ Dblite.prototype.searchVideosByInterval = function( start, end, cb ) {
 
 Dblite.prototype.getExpiredChunks = function( expirationDate, numberOfChunks, cb ) {
     var self = this;
-	var query = 'SELECT * FROM videos WHERE start < ? ORDER BY id ASC LIMIT ?';
+	var query = 'SELECT id, file, start FROM videos WHERE start < ? ORDER BY id ASC LIMIT ?';
 
     var fileList = this.db.query(
 			query, 
