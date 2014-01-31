0795d7883a7584684d78894d178fb60d787a6c8d
Chris Waddington <cwadding@gmail.com>
2014-01-31 14:01:12 -0500

    fixed test in file backup

--- app/tests/file_backup_test.js
+++ app/tests/file_backup_test.js
@@ -14,9 +14,9 @@ describe('FileBackup', function(){
 	    	var expected_backups = [ { name: 'cam_idx.sqlite_1390849671269.backup', time: 1390849671269 },
 { name: 'cam_idx.sqlite_1390849971274.backup',time: 1390849971274 } ];
 			backup.setup(function(actual_backups){
-				assert(actual_backups.length > 0);
-				for (var idx in actual_backups){
-					assert.deepEqual(actual_backups[idx], expected_backups[idx]);
+				assert(actual_backups.backups.length > 0);
+				for (var idx in actual_backups.backups){
+					assert.deepEqual(actual_backups.backups[idx], expected_backups[idx]);
 				}
 				done();
 			});
