e36665927e4ab91606e7ec4f725a20e4b37b2bb8
manuel <manuel@debian>
2014-03-17 14:59:47 -0400

    working time marker on camera page

---
+++ app/assets/js/cam_init.js
@@ -0,0 +1,429 @@
+function CameraPage( camId ) {
+
+	this.mouseX = 0;
+	this.mouseY = 0;
+
+	this.camId = camId;
+	this.streamId = '';
+
+	this.inputs  = {};
+	this.buttons = {};
+
+	this.inputs.date    = $('#begin_date');
+	this.inputs.streams = $('#stream-selector');
+	
+	this.buttons.download   = $('#download');
+	this.buttons.snapshot   = $('#snapshot');
+	this.buttons.livestream = $('#get-livestream');
+
+	this.marker = $('#marker');
+
+	this.indexer = new Indexer();
+	this.player = new Player('#video');
+};
+
+
+CameraPage.prototype.setup = function() {
+
+	var self = this;
+
+	this.getStreamsInfo( function() {
+		self.setupSocket();
+		self.setupDatepicker();
+		self.setupStreamSelector();
+		self.setupButtons();
+		self.setupTimeline();
+		self.setupEvents();
+	});
+};
+
+
+CameraPage.prototype.setupEvents = function() {
+
+	var self = this;
+	
+	window.onCurrentTimeChange = function (time, playerId) {
+		var timelineWidth = $("#timeline").width();	
+	    var totalTime = 24*60*60;
+		var absolute_time = self.indexer.getAbsoluteTime( time );
+		var dt = ( absolute_time - self.timeline.begin )/1000; 
+		var pos = dt * (1.0 * timelineWidth) / totalTime - 2;
+		self.marker.css("left", pos);
+ 	}
+
+	$(document).mousemove(function(e){
+
+		self.mouseX = e.pageX;
+		self.mouseY = e.pageY;
+
+		if (self.mouseX < $(window).width()/2) { 
+			$("#thumb").css('left', (self.mouseX+10)+'px');
+		} else {
+			$("#thumb").css('left', (self.mouseX-170)+'px');
+		}
+		if (self.mouseY < $(window).height()/2) {
+			$("#thumb").css('top', (self.mouseY+10)+'px');
+		} else {
+			$("#thumb").css('top', (self.mouseY-130)+'px');
+		}
+	});
+};
+
+
+CameraPage.prototype.updateTimelines = function() {
+
+	if (!timeline) {
+		console.log("ERROR: no such timeline");
+		console.log(data);
+		return;
+	}
+
+	var thumb = "/cameras/" + data.cam + "/streams/" + data.stream + "/thumb/" + data.thumb;
+
+	var img = new Image();
+		
+	var showThumbWrapper = function(d) { 
+		showThumb(d.attr("data-thumb"));
+		updateTime( parseInt(d.attr('data-start')) );
+	};
+
+	timeline.append({
+		start:       data.start,
+		w:           data.end - data.start,
+		thumb:       thumb,
+		totalTime:   data.totalTime,
+		mouseover:   showThumbWrapper,
+		mouseclick:  jumpTo
+	});
+};
+
+
+CameraPage.prototype.setupTimeline = function() {
+
+	$('#timeline').html('');
+
+	var timelineContainer = $("<div>", {
+		id:     "timeline-container",
+		class:  "timeline-container"
+	});
+
+	timelineContainer.appendTo("#timeline")
+		.mouseleave( function() {
+			$("#thumb").hide();
+		}
+	);
+
+	this.timeline = new Timeline("#timeline-container", { 
+		static:    true,
+		seekable:  true,
+		timeSpan:  24*60*60*1000
+	});
+
+};
+
+
+CameraPage.prototype.setupSocket = function() {
+
+	this.socket = io.connect();
+
+	this.socket.on('newThumb', function(data) {
+	//	updateThumb( data );
+	});
+
+	this.socket.on('newChunk', function (data) {
+		// console.log(data);
+		// $("#timeline-"+data.stream).animate({
+		// 	backgroundColor: "none"
+		// }, 1000);
+	//	updateTimelines(data, {updateThumb: false});
+	});
+};
+
+
+CameraPage.prototype.setupDatepicker = function() {
+
+	var self = this;
+	var dateInput = this.inputs.date;
+
+	dateInput.pickadate();
+
+	dateInput.change( function() {
+						
+		var begin_date = dateInput.val();
+		if (!begin_date) return;
+
+		begin_date   = new Date( begin_date );
+		var end_date = new Date( begin_date );
+
+		end_date.setHours(23);
+		end_date.setMinutes(59);
+		end_date.setSeconds(59);
+
+		begin_date = Date.parse( begin_date );
+		end_date   = Date.parse( end_date );
+
+		self.play(begin_date, end_date);	
+		// console.log('setupDatePicker()');
+		// playVideo( camId, streamId, begin_date, end_date );
+	});
+};
+
+
+CameraPage.prototype.setupStreamSelector = function() {
+
+	var self = this;
+
+	self.streamId = self.inputs.streams.val();
+	
+	// debugger;
+	this.inputs.streams.change( function() { 
+
+		var begin_date = self.inputs.date.val();
+
+		if (!begin_date) return;
+		begin_date = new Date( begin_date );
+		var end_date = new Date(begin_date);
+
+		end_date.setHours(23);
+		end_date.setMinutes(59);
+		end_date.setSeconds(59);
+
+		begin_date = Date.parse( begin_date );
+		end_date   = Date.parse( end_date );
+
+		self.streamId = self.inputs.streams.val();
+		self.play( begin_date, end_date );
+	});				
+};
+
+
+CameraPage.prototype.setupButtons = function() {
+
+	var self = this;
+
+	// download button
+	this.buttons.download.click(function() {
+
+		var stream = self.inputs.streams.val();
+		var begin  = Date.parse( self.inputs.date.val() );
+		var end    = new Date();
+
+		if (!begin) {
+			return;
+		}
+
+		end.setHours(23);
+		end.setMinutes(59);
+		end.setSeconds(59);
+		
+		end = Date.parse( end );
+
+		var url = window.location.origin 
+				+ "/cameras/" + self.camId
+				+ "/download?begin=" + begin
+				+ "&end=" + end 
+				+ "&stream=" + stream;
+		window.location = url;
+	});
+
+	// snapshot button
+	this.buttons.snapshot.click(function() {
+		// $("#file-list").html("<span class='subtle'>loading...</span>");
+		// var time = 1000*moment( self.inputs.date.val() ).unix(); 
+		// getSnapshot(time);
+		
+	});
+
+	// livestream button
+	this.buttons.livestream.click(function() {
+		// showLiveStream();
+	});
+};
+
+
+CameraPage.prototype.getStreamsInfo = function(cb) {
+
+	var self = this;
+
+	$.ajax({
+		url: '/cameras/' + self.camId + '/json',
+		success: function(data) {
+			if (!data.camera.streams) {
+				if (cb) cb("no streams");
+				return;
+			} else {
+				streams = data.camera.streams;
+				self.populateStreamSelector( data.camera.streams );	
+				if (!data.camera.name) {
+					$("#camera_name").html(data.camera.manufacturer);
+				}
+				if (cb) cb(data.camera);
+			}
+		},
+		error: function(err) {
+			console.log( err );
+		}
+	});
+};
+
+
+CameraPage.prototype.populateStreamSelector = function(streams) {
+	
+	for (var s in streams) {
+
+		var text = '';
+
+		if ( streams[s].name ) {
+			text = text + streams[s].name + ' - ';
+		} if ( streams[s].resolution ) {
+			text = text + streams[s].resolution;
+		} else {
+			text = text + ' ' + streams[s].url;
+		}
+		
+        this.inputs.streams.append($('<option>', { value: streams[s].id })
+          .text( text ));		
+	}
+};
+
+
+CameraPage.prototype.loadIndexer = function( begin, end, cb ) {
+
+	var self = this;
+
+	this.indexer.clear();
+
+    $.getJSON(	"/cameras/" + this.camId + 
+				"/streams/" + this.streamId + 
+				"/list_videos?start=" + begin +
+				"&end=" + end, 
+		function( data ) {
+
+			for (var i = 0; i < data.videos.length; i++) {
+				
+				var start = data.videos[i].start;
+				var end   = data.videos[i].end;
+
+				self.indexer.push( data.videos[i] );	
+			}
+			if(cb) cb();
+		}
+	);
+};
+
+
+CameraPage.prototype.play = function( begin, end ) {
+
+	var self = this;
+
+	this.loadIndexer( begin, end, function() {
+		self.launchTimeline( 50, begin, end);
+		self.player.playVideo( self.camId, self.streamId, begin, end);
+	});
+}
+
+
+CameraPage.prototype.launchTimeline = function( block_size, begin, end) {
+
+	var self = this;
+
+	this.timeline.clear();
+
+	block_size = block_size || 2;
+	var elements = this.indexer.agglutinate(block_size);
+	
+	this.timeline.setEnd( end );
+	this.timeline.setBegin( begin );
+
+	for( var i in elements ) {
+
+		var start = elements[i].start;
+		var end   = elements[i].end;
+	
+		self.updateTimelines({
+			cam:        self.camId,
+			stream:     self.streamId,
+			start:      start,
+			end:        end,
+			totalTime:  elements[i].totalTime,
+			thumb:      elements[i].thumb
+		});
+	}
+}
+
+
+CameraPage.prototype.updateTimelines = function( data, options ) {
+
+	var self = this;
+
+	if (!self.timeline) {
+		console.log("ERROR: no such timeline");
+		return;
+	}
+
+	var thumb = "/cameras/" + this.camId + "/streams/" + this.streamId + "/thumb/" + data.thumb;
+
+	var img = new Image();
+		
+	var showThumbWrapper = function(d) { 
+		self.showThumb(d.attr("data-thumb"));
+		self.updateTime( parseInt(d.attr('data-start')) );
+	};
+
+	self.timeline.append({
+		start:       data.start,
+		w:           data.end - data.start,
+		thumb:       thumb,
+		totalTime:   data.totalTime,
+		mouseover:   showThumbWrapper,
+		mouseclick:  self.jumpTo
+	});
+}
+
+
+CameraPage.prototype.updateTime = function( time ) {
+
+	var t       = new Date(time);
+	var hours   = t.getHours();
+	var minutes = t.getMinutes();
+	var seconds = t.getSeconds();
+
+	if ( parseInt(seconds) < 10) seconds = '0' + seconds;
+	if ( parseInt(minutes) < 10) minutes = '0' + minutes;
+
+	var formattedTime = hours + ':' + minutes + ':' + seconds;
+	$('#thumb-time').html(formattedTime);
+};
+
+
+CameraPage.prototype.showThumb = function( thumb ) {
+   
+	var currentThumb = $("#thumb img").attr('src');
+
+	$("#thumb").show();
+
+	this.lastThumbRequest = this.lastThumbRequest || Date.now() - 1000;
+	var dt = Date.now() - this.lastThumbRequest;
+	
+	if (currentThumb !== thumb && dt > 100) {
+		this.lastThumbRequest = Date.now();
+		$("#thumb img").attr('src', thumb);
+	} else {
+	}
+}
+
+
+CameraPage.prototype.jumpTo = function(d) {
+	
+	var player = document.getElementById("strobeMediaPlayback");
+	var time   = parseInt( d.attr('data-totalTime') );
+// 	var dt        = time - timeline_begin;
+// 	q
+// 	var totalTime = 30*60*1000;
+//
+// 	var time = dt/1000;
+	if (player.canSeekTo(time) ) {
+		console.log("seek to " + time);
+		player.seek(time);
+	}
+}
---
+++ app/assets/js/player.js
@@ -0,0 +1,212 @@
+function Player( el ) {
+
+	this.layers = {};
+	
+	this.layers.livePlayer = $("<div>", {
+		id:      'live-player',
+		class:   'player-layer'
+	}).appendTo(el);
+
+	this.layers.nativePlayer = $("<video>", {
+		id:      'native-player',
+		class:   'player-layer'
+	}).appendTo(el);
+	
+	this.layers.snapshot = $("<div>", {
+		id:      'snapshot',
+		class:   'player-layer'
+	}).appendTo(el);
+
+	this.layers.strobePlayer = $("<object>", {
+		id:      'strobeMediaPlayback',
+		class:   'player-layer'
+	}).appendTo(el);
+
+	this.layers.strobePlayer.html(
+		"<p>( this player requires flash. check if it's enabled and make sure your browser supports flash )</p>"
+	);
+}
+
+
+/**
+ * Launches native player 
+ *		runs on safari (osx/ios) / some android devices
+ */
+Player.prototype.launchNativePlayer = function( url ) {
+
+	for( var i in this.layers ) {
+		this.layers[i].hide();
+	}
+	this.layers.nativePlayer.show();
+	this.layers.nativePlayer.attr("src", url);
+};
+
+
+/**
+ * Launches vlc plugin player
+ *		doesnt run well on osx 
+ */
+Player.prototype.showLiveStream = function( url ) {
+	
+	for( var i in this.layers ) {
+		this.layers[i].hide();
+	}
+
+	var html = '<embed type="application/x-vlc-plugin"' +
+				'name="102"' +
+				'autoplay="yes" width="640" height="480"' +
+				'target="'+url+'" />';
+
+	this.layers.livePlayer.html( html );
+	this.layers.livePlayer.show();
+};
+
+
+/**
+ * Launches strobe player
+ *  
+ */
+Player.prototype.launchStrobePlayer = function( options ) {
+
+	for( var i in this.layers ) {
+		this.layers[i].hide();
+	}
+
+	this.layers.strobePlayer.show();
+
+	options.url = encodeURIComponent( options.url );
+
+	var parameters = {
+		src:                         options.url,
+		autoPlay:                    options.autoplay,
+		verbose:                     true,
+		controlBarAutoHide:          "true",
+		controlBarPosition:          "bottom",
+		poster:                      "",
+		plugin_hls:                  "/swf/HLSDynamicPlugin.swf",
+		javascriptCallbackFunction:  "window.onJavaScriptBridgeCreated"
+	};
+
+	var wmodeValue = "direct";
+	var wmodeOptions = ["direct", "opaque", "transparent", "window"];
+
+	if ( parameters.hasOwnProperty("wmode") ) {
+		if (wmodeOptions.indexOf(parameters.wmode) >= 0) {
+			wmodeValue = parameters.wmode;
+		}	            	
+		delete parameters.wmode;
+	}
+
+	// Embed the player SWF:	            
+	swfobject.embedSWF(
+			"/swf/StrobeMediaPlayback.swf"
+			, "strobeMediaPlayback"
+			, 640
+			, 480
+			, "10.1.0"
+			, "/swf/expressInstall.swf"
+			, parameters
+			, {
+				allowFullScreen: "true",
+				wmode: wmodeValue
+			}
+			, {
+				name: "strobeMediaPlayback"
+			}
+	);
+
+// 	window.onCurrentTimeChange = function (time, playerId) {
+// 		var timelineWidth = $("#timeline").width();	
+// 	    var totalTime = 24*60*60;
+// 		var absolute_time = indexer.getAbsoluteTime( time );
+// 		var dt = ( absolute_time - timeline.begin )/1000; 
+// 		var pos = dt * (1.0 * timelineWidth) / totalTime - 2;
+// 		$("#marker").css("left", pos);
+// 	}
+// 			
+// 	window.onDurationChange = function (time, playerId) {
+// 	}
+//
+	var player;
+	window.onJavaScriptBridgeCreated = function(playerId) {
+		if (!player ) {
+			player = document.getElementById(playerId);
+			// Add event listeners that will update the 
+			player.addEventListener("currentTimeChange" , "window.onCurrentTimeChange");
+			player.addEventListener("durationChange"    , "window.onDurationChange");
+
+			// Pause/Resume the playback when we click the Play/Pause link
+			document.getElementById("play-pause").onclick = function(){
+				var state = player.getState();
+				if (state == "ready" || state == "paused") {
+					player.play2();
+				}
+				else 
+					if (state == "playing") {
+						player.pause();
+					}
+				return false;
+			};
+		}
+	}
+};
+
+
+/**
+ * Jumps to specified time  
+ *	@param { time } Number
+ *		elapsed time in seconds
+ */
+Player.prototype.jumpTo = function( time ) {
+	
+	var player = document.getElementById("strobeMediaPlayback");
+
+	if (player.canSeekTo(time) ) {
+		console.log("seek to " + time);
+		player.seek(time);
+	}
+};
+
+
+/**
+ * Plays video using compatible player 
+ *  if platform doesnt support native hls, uses flash player instead
+ *
+ *	@param { stream } String
+ *		stream id
+ *	@param { begin } Number
+ *		begin time (utc millisecs)
+ *	@param { end } Number
+ *		end time (utc millisecs)
+ *  
+ */
+Player.prototype.playVideo = function( camId, streamId, begin, end ) { 
+	
+	// loadIndexer( begin, end, function() {
+	// 	launchTimeline( 50, begin, end);
+	// });
+		
+	var url = window.location.origin +
+	   	"/cameras/" + camId + 
+		"/video.m3u8?begin=" + begin +
+		"&end=" + end +
+		"&stream=" + streamId;
+
+	if (!this.canPlayHLS()) {
+		this.launchStrobePlayer({
+			url:       url,
+			autoplay:  true
+		});
+	} else {
+		this.launchNativePlayer( url );
+	}
+};
+
+
+/**
+ * Checks if broser supports hls natively
+ *
+ */
+Player.prototype.canPlayHLS = function() {
+	return document.createElement('video').canPlayType('application/vnd.apple.mpegURL') === 'maybe';
+};
