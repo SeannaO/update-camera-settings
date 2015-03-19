ce75f7636bd2e9ada451bb58aa7b8059334a90ee
manuel <guerra.manuel@gmail.com>
2015-03-19 17:16:47 -0400

    add 'test' alias to Grunt

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -4,6 +4,8 @@ module.exports = function(grunt) {
 	require('time-grunt')(grunt);
 	require('load-grunt-tasks')(grunt);
 	require('load-grunt-config')(grunt);
+	
+	grunt.registerTask('test', ['mocha_istanbul:coverage']);
   
 };
 
