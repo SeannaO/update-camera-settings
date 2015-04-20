c8b631e095b1dae1a12ecdb550e5fa9fddb5dea3
manuel <guerra.manuel@gmail.com>
2015-04-20 10:31:13 -0400

    [investigator]  add toggle and calendar buttons

--- app/assets/js/investigator/src/investigator.js
+++ app/assets/js/investigator/src/investigator.js
@@ -1,7 +1,25 @@
+var React = require('react/addons');
+
+var CameraGrid       = require('./camera-grid.js');
+var ToggleCameraList = require('./toggle-cameras.js');
+var CamerasListBox   = require('./cameras-list.js');
+var Datepicker       = require('./datepicker.js');
+
 React.render(
 	<div>
+		<div className='onTop'>
+			<Datepicker/>
+		</div>
 		<CameraGrid/>
-		<CamerasListBox url='/cameras.json' />
+		<CamerasListBox url='/cameras.json' show={false} />
 	</div>,
 	document.getElementById('main')
 );
+
+React.render(
+	<ToggleCameraList />,
+	document.getElementById('toolbar')
+);
+
+
+
