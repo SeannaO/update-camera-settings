5a8a2109ac2196292aed4b318636a7b66c6f5c64
manuel <guerra.manuel@gmail.com>
2015-04-22 13:01:17 -0400

    [investigator]  include timeline component

--- app/assets/js/investigator/src/investigator.js
+++ app/assets/js/investigator/src/investigator.js
@@ -4,6 +4,7 @@ var CameraGrid       = require('./camera-grid.js');
 var ToggleCameraList = require('./toggle-cameras.js');
 var CamerasListBox   = require('./cameras-list.js');
 var Datepicker       = require('./datepicker.js');
+var Timeline         = require('./timeline-component.js');
 
 React.render(
 	<div>
@@ -21,5 +22,8 @@ React.render(
 	document.getElementById('toolbar')
 );
 
-
+React.render(
+	<Timeline />,
+	document.getElementById('timeline-container')
+);
 
