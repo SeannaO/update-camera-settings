69f3a79d19e287f267eb1554f6f93401d4185df8
manuel <guerra.manuel@gmail.com>
2015-04-27 14:25:35 -0400

    [investigator]  add thumbnail component

---
+++ app/assets/js/investigator/src/thumbnail-component.js
@@ -0,0 +1,80 @@
+var React = require('react/addons');
+var moment = require('moment');
+
+var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
+
+
+var ThumbnailPreview = React.createClass({
+	
+
+	mixins: [
+		PureRenderMixin
+	],
+
+	componentDidMount: function() {
+		
+	},
+
+
+	formatThumbs: function() {
+
+		this.thumbsCounter = 0;
+
+		if (!this.props.thumbs) return;
+		var el = [];
+		for (var t of this.props.thumbs) {
+
+			if (t) this.thumbsCounter++;
+
+			el.push( 
+					<div className = 'thumb'>
+						<img src={t}/>
+					</div>
+				   );
+		}
+		return el;
+	},
+
+
+	addZero: function() {
+	},
+
+
+	formatTime: function() {
+		var t = moment( this.props.time ).format('h:mm:ss a');
+		return t;
+	},
+
+
+	render: function() {
+
+		var thumbsEl = this.formatThumbs();
+
+		var shouldBeVisible = this.thumbsCounter > 0 && this.props.visible;
+
+		var thumbPos = {
+			left:     this.props.px + 25,
+			top:      this.props.py + 25,
+			display:  shouldBeVisible ? '' : 'none',
+		};
+
+		var style = 'shadow thumbPopover';
+
+		return(
+				<div
+					className = {style}
+					style     = {thumbPos}
+				>
+					<center>
+						<div id = 'thumbTime'>
+								{this.formatTime()}
+						</div>
+					</center>
+					{thumbsEl}
+				</div>
+		);
+	},
+});
+
+
+module.exports = ThumbnailPreview;
