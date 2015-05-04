67effe5c189367c4f98b9d95f3a989a7d471075e
manuel <guerra.manuel@gmail.com>
2015-05-04 16:37:47 -0400

    [investigator]  zoom out button

---
+++ app/assets/js/investigator/src/toolbar/zoom-out.js
@@ -0,0 +1,61 @@
+var React = require('react/addons');
+var bus   = require('../event-service.js');
+
+
+var ZoomOutButton = React.createClass({
+
+	handleClick: function() {
+		bus.emit('zoom-out', {
+		});
+	},
+
+	getInitialState: function() {
+		return({
+			visible: this.props.visible
+		});
+	},
+
+	show: function() {
+		this.setState({
+			visible: true
+		});
+	},
+
+	hide: function() {
+		this.setState({
+			visible: false
+		});
+	},
+
+	componentWillUnmount: function() {
+		bus.removeListener('day-selected', this.hide);
+		bus.removeListener('no-more-zoom-out', this.hide);
+		bus.removeListener('zoom-in', this.show);
+	},
+
+	componentDidMount: function() {
+		bus.on('day-selected', this.hide);
+		bus.on('no-more-zoom-out', this.hide);
+		bus.on('zoom-in', this.show);
+	},
+
+	render: function() {
+
+		var style = {
+			marginLeft: '15px',
+			display: this.state.visible ? '' : 'none'
+		};
+
+		var className = 'btn-highlight glyphicon glyphicon-zoom-out';
+		return(
+			<span
+				style     = {style}
+				className = {className}
+				onClick   = {this.handleClick}
+			/>
+		);
+	}
+});
+
+
+module.exports = ZoomOutButton;
