0115e67c715cab7351167d93e63b72b73979c7e0
manuel <guerra.manuel@gmail.com>
2015-05-06 17:38:37 -0400

    [investigator]  move some event handlers to main component

--- app/assets/js/investigator/src/timeline/timeline-event-handler-mixin.js
+++ app/assets/js/investigator/src/timeline/timeline-event-handler-mixin.js
@@ -11,9 +11,6 @@ var TimelineEventHandlerMixin = {
 		bus.on('camera-metadata', this.handleCameraMetadata);
 		bus.on('day-selected', this.handleDateChange);
 		bus.on('skip', this.handleSkip);
-		bus.on('go-live', this.handleGoLive);
-		bus.on('download-video', this.handleDownload);
-		bus.on('go-to-camera', this.goToCamera);
 	},
 
 	componentWillUnmount: function() {
@@ -23,66 +20,6 @@ var TimelineEventHandlerMixin = {
 		bus.removeListener('camera-metadata', this.handleCameraMetadata);
 		bus.removeListener('day-selected', this.handleDateChange);
 		bus.removeListener('skip', this.handleSkip);
-		bus.removeListener('go-live', this.handleGoLive);
-		bus.removeListener('download-video', this.handleDownload);
-		bus.removeListener('go-to-camera', this.goToCamera);
-	},
-
-	goToCamera: function(d) {
-
-		if (!this.state.begin) return;
-		if (!this.state.end) return;
-		if (!d.id) return;
-		
-		var offset = this.state.time;
-		if (isNaN(offset)) return;
-
-		var url = window.location.origin + 
-			'/cameras/' + d.id + 
-			'?time=' + Math.round(this.state.time) + 
-			'&begin=' + Math.round(this.state.begin) +
-			'&end=' + Math.round(this.state.end);
-
-		window.open( url );
-	},
-
-	handleDownload: function(d) {
-
-		if (!this.state.begin || !this.state.end ) {
-			return;
-		} 
-
-		if (this.state.end - this.state.begin > 2*60*60*1000) {
-			toastr.warning('Please select an interval shorter than 2h');
-			return;
-		}
-
-		var url = window.location.protocol + "//" + window.location.host
-			+ "/cameras/" + d.id
-			+ "/download?begin=" + parseInt( this.state.begin )
-			+ "&end=" + parseInt( this.state.end );
-
-		var begin = new Date(this.state.begin),
-			end   = new Date(this.state.end);
-
-		bootbox.confirm('Download video<br><br> <b>FROM</b>: ' + begin + '<br> <b>TO</b>: ' + end  + '<br><br> Confirm? ', function(ok) {
-			if(!ok) {
-				return;	
-			} else {
-				var w = window.open( url );
-				window.focus();
-				w.onload = function() {
-					if (w.document.body.innerHTML.length > 0) {
-						w.close();
-						if (w.document.body.innerHTML.indexOf('long') >= 0) {
-							toastr.error('requested video is too long, please select a shorter interval');
-						} else {
-							toastr.error('couldn\'t find the requested video');
-						}
-					}
-				};
-			}
-		});
 	},
 
 	handleGoLive: function(d) {
@@ -106,6 +43,11 @@ var TimelineEventHandlerMixin = {
 		var end = Date.parse(date);
 
 		this.setState({
+			begin:  begin,
+			end:    end
+		});
+
+		bus.emit('interval-change', {
 			begin:  begin,
 			end:    end
 		});
