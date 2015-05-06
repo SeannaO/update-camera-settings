be5b819fa303688515c15a64517e91d3fad5f24b
manuel <guerra.manuel@gmail.com>
2015-05-06 12:44:23 -0400

    [investigator]  investigator component

--- app/assets/js/investigator/src/investigator.js
+++ app/assets/js/investigator/src/investigator.js
@@ -3,40 +3,32 @@ var React = require('react/addons');
 var CameraGrid       = require('./camera-grid.js');
 var ToggleCameraList = require('./toggle-cameras.js');
 var CamerasListBox   = require('./cameras-list.js');
-var Datepicker       = require('./toolbar/datepicker.js');
 var Timeline         = require('./timeline/timeline-component.js');
-var CurrentTime      = require('./toolbar/current-time.js');
-var TogglePlay       = require('./toolbar/toggle-play.js');
+var Toolbar          = require('./toolbar/toolbar.js');
 
-React.render(
-	<div>
-		<CameraGrid/>
-		<CamerasListBox url='/cameras.json' show={false} />
-	</div>,
-	document.getElementById('main')
-);
+var Investigator = React.createClass({
 
-React.render(
-	<ToggleCameraList />,
-	document.getElementById('toolbar')
-);
+	componentDidMount: function() {
+	},
 
-React.render(
-	<Timeline />,
-	document.getElementById('timeline-container')
-);
+	render: function() {
+		return (
+			<div>
+				<div id = "grid">
+					<CameraGrid/>
+					<CamerasListBox url='/cameras.json' show={false} />
+				</div>
 
-React.render(
-	<Datepicker/>,
-	document.getElementById('datepicker-button')
-);
+				<div id = 'timeline-toolbar-container'>
+					<Toolbar/>
+				</div>
 
-React.render(
-	<CurrentTime/>,
-	document.getElementById('current-time')
-);
+				<div id = 'timeline-container'>
+					<Timeline />
+				</div>
+			</div>
+		)
+	}
+});
 
-React.render(
-	<TogglePlay/>,
-	document.getElementById('toggle-play')
-);
+module.exports = Investigator;
--- app/assets/js/investigator/src/main.js
+++ app/assets/js/investigator/src/main.js
@@ -1,34 +1,8 @@
 var React = require('react/addons');
 
-var CameraGrid        = require('./camera-grid.js');
-var ToggleCameraList  = require('./toggle-cameras.js');
-var CamerasListBox    = require('./cameras-list.js');
-var Timeline          = require('./timeline/timeline-component.js');
-var Toolbar           = require('./toolbar/toolbar.js');
+var Investigator = require('./investigator.js');
 
 React.render(
-	<div>
-		<CameraGrid/>
-		<CamerasListBox url='/cameras.json' show={false} />
-	</div>,
+	<Investigator/>,
 	document.getElementById('main')
 );
-
-
-React.render(
-	<ToggleCameraList />,
-	document.getElementById('toolbar')
-);
-
-
-React.render(
-	<Toolbar/>,
-	document.getElementById('timeline-toolbar-container')
-);
-
-
-React.render(
-	<Timeline />,
-	document.getElementById('timeline-container')
-);
-
