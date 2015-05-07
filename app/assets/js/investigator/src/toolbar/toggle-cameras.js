42212c8ac965199f987ca8a113fe29a0f1ceec54
manuel <guerra.manuel@gmail.com>
2015-05-07 11:33:10 -0400

    [investigator]  move toggle cameras button to toolbar folder

---
+++ app/assets/js/investigator/src/toolbar/toggle-cameras.js
@@ -0,0 +1,30 @@
+var React                = require('react/addons');
+var mui                  = require('material-ui');
+var FloatingActionButton = mui.FloatingActionButton;
+var bus                  = require('../services/event-service.js');
+
+var ToggleCameraList = React.createClass({
+
+	toggle: function() {
+		bus.emit('toggle-cameras-list');	
+	},
+
+	componentDidRender: function() {
+		console.log('ok');
+	},
+
+	render: function() {
+
+		return (
+			<div
+				onClick   = {this.toggle}
+				className = 'noselect btn-highlight'
+			>
+				<span className = 'glyphicon glyphicon-facetime-video'>
+				</span>
+			</div>
+		);
+	}
+});
+
+module.exports = ToggleCameraList;
