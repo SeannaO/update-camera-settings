680a27d2669d53639acd3c822a178530714741a8
Manuel Guerra <guerra.manuel@gmail.com>
2015-07-21 17:22:24 -0400

    add SolinkConnect init.d file

---
+++ SolinkConnect.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+CONF=/etc/config/qpkg.conf
+QPKG_NAME="SolinkConnect"
+QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
+
+export PATH=$PATH:$QPKG_ROOT/app:/opt/bin:/opt/sbin
+export LD_LIBRARY_PATH=$LD_LBRARY_PATH:$QPKG_ROOT/app/bin/lib
+
+case "$1" in
+  start)
+    ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
+    if [ "$ENABLED" != "TRUE" ]; then
+        echo "$QPKG_NAME is disabled."
+        exit 1
+    fi
+    : ADD START ACTIONS HERE
+    cd $QPKG_ROOT/app
+    ./node_modules/forever/bin/forever --sourceDir=$QPKG_ROOT/app start ./app.js "/share/SolinkConnect" -development
+    ./node_modules/forever/bin/forever start $QPKG_ROOT/app/server.js
+    ;;
+
+  stop)
+    : ADD STOP ACTIONS HERE
+    cd $QPKG_ROOT/app
+    ./node_modules/forever/bin/forever stop $QPKG_ROOT/app/app.js 
+    ./node_modules/forever/bin/forever stop $QPKG_ROOT/app/server.js
+    ;;
+
+  restart)
+    $0 stop
+    $0 start
+    ;;
+
+  *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+
+exit 0
