f03f51b5cd3b4143047fb5a97e08edef51a9e16e
fperks <fperks@solinkcorp.com>
2016-05-17 15:48:17 -0400

    Update key

--- app/config/index.js
+++ app/config/index.js
@@ -5,23 +5,23 @@ var fs = require('fs');
 
 var all = {
 
-  https_private_key: fs.readFileSync(path.join(process.cwd(), 'assets/private.pem')),
-  https_public_key: fs.readFileSync(path.join(process.cwd(), 'assets/public.pem')),
+    https_private_key: fs.readFileSync(path.join(process.cwd(), 'assets/solinkdirect.key')),
+    https_public_key: fs.readFileSync(path.join(process.cwd(), 'assets/solinkdirect.cert')),
 
-	http_ports: {
-		main: process.env.HTTP_PORT || 9080,
-		secondary: [
-			4001,
-			4002,
-			4003,
-			4004
-		]
-	},
+    http_ports: {
+        main: process.env.HTTP_PORT || 9080,
+        secondary: [
+            4001,
+            4002,
+            4003,
+            4004
+        ]
+    },
 
-	// HTTPS does not have multiple ports for now
-	https_ports: {
-		main: process.env.PORT || 8080
-	},
+    // HTTPS does not have multiple ports for now
+    https_ports: {
+        main: process.env.PORT || 8080
+    },
 };
 
 module.exports = all;
