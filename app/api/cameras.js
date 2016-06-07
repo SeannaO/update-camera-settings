7f4f6e79887b7d2c5469d9c1bfc6b60fe16688ea
Manuel Guerra <guerra.manuel@gmail.com>
2016-06-07 11:43:12 -0400

    [api/cameras]  add alternate subtitles download endpoint

--- app/api/cameras.js
+++ app/api/cameras.js
@@ -468,6 +468,59 @@ module.exports = function( app, passport, camerasController ) {
 	});
 	// - - -
 
+
+	app.get('/cameras/:id/subtitles', passport.authenticate('basic', {session: false}), function(req, res) {
+
+            var camId  = req.params.id;
+            var begin  = parseInt( req.query.begin, 10 );
+            var end    = parseInt( req.query.end, 10 );
+            var stream = req.query.stream;
+
+            if (!begin || !end || !camId) {
+                res.status(422).json( { error: 'invalid request' } );
+                return;
+            }
+            if (begin > end ) {
+                res.status(422).json({error:'begin time greater than end time'});
+                return;
+            }
+            if ( end - begin > 5 * 60 * 60 * 1000 ) {
+                res.status(422).json({error:'video requested is too long ( > 5h )'});
+                console.error('[api/subtitles] attempt to download subtitles for video longer than 5h');
+                return;
+            }
+
+            var self = this;	
+            self.stream = stream;
+
+            camerasController.getCamera( camId, function(err, cam) {
+                if (err || !cam) {
+                    err = err || 'camera  ' + camId + ' not found';
+                    console.error("[api/subtitles]: " + err);
+                    res.status(404).json({ error: err });
+                } else {
+                    if ( cam.streams.length == 0 ) {
+                        console.error("[api/subtitles]  camera does not have any streams");
+                        res.status(422).json({ error: 'camera does not have any streams' });
+                        return;
+                    }
+                    // in case the streamId is invalid or not specified
+                    if ( !self.stream || !cam.streams[self.stream] ) {
+                        console.error("[/cameras/:id/download]  no such stream ");
+                        for (var s in cam.streams){
+                            self.stream = s;
+                            break;
+                        }
+                    }
+
+                    // enforce subtitle format
+                    req.query.format = 'srt';
+                    camerasController.mp4Handler.inMemoryMp4Video( cam.streams[self.stream].db, cam, begin, end, req, res );
+                }
+            });
+	});
+	// - - -
+
 	// - - -
 	// gets inMem mp4 video
 	app.get('/cameras/:id/download', passport.authenticate('basic', {session: false}), function(req, res) {
